feb9b211e1b88919ba76b94a8dcb5ed740101aea
Lucas Bachega <lucas.cbachega@gmail.com>
2025-06-11 18:09:32 -0300

    configure AppContainer and theme control

---
+++ src/components/layout/AppContainer.tsx
@@ -0,0 +1,24 @@
+import { Box, useColorScheme } from "@mui/material";
+import { useEffect } from "react";
+
+const AppContainer = ({ children }: any) => {
+  const { mode } = useColorScheme();
+
+  useEffect(() => {
+    if (mode === "light") {
+      document.body.style.backgroundColor = "#ffffff";
+    } else {
+      document.body.style.backgroundColor = "#121212";
+    }
+  }, [mode]);
+
+  return (
+    <div className="App">
+      <Box flex={1} bgcolor={"background.default"} color={"text.primary"}>
+        {children}
+      </Box>
+    </div>
+  );
+};
+
+export default AppContainer;
---
+++ src/theme/index.ts
@@ -0,0 +1,58 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: "class",
+    disableCssColorScheme: true,
+  },
+  typography: {
+    fontFamily: '"Inter", sans-serif',
+    allVariants: {
+      fontWeight: 500,
+    },
+    button: {
+      textTransform: "none",
+      fontWeight: 600,
+      fontSize: ".975rem",
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        color: "text.primary",
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true,
+      },
+      styleOverrides: {
+        root: {
+          transition: "none",
+          borderRadius: "100px",
+        },
+      },
+    },
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        text: {
+          primary: "#0D0D0D",
+          secondary: "#69666F",
+        },
+      },
+    },
+    dark: {
+      palette: {
+        text: {
+          primary: "#FFF",
+          secondary: "#EBE9F0",
+        },
+      },
+    },
+  },
+});
